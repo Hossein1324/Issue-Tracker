fed91aa367e458df8a392655ef3d1e305cbffd9a
Hossein <hosseinpro@gmail.com>
2024-09-27 23:11:56 +0330

    Build the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
-import Image from 'next/image'
-import LatestIssues from './LatestIssues'
+import prisma from '@/prisma/client';
+import LatestIssues from './LatestIssues';
+import IssueChart from './IssueChart';
+import { Grid } from '@radix-ui/themes';
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma.problem.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.problem.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.problem.count({
+    where: { status: 'CLOSED' },
+  });
+
   return (
-    <LatestIssues />
+    <Grid columns={{ initial: '1', sm: '2' }} gap='8' >
+      <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      <LatestIssues />
+
+    </Grid>
   )
 }
