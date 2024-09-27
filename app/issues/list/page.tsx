0e0105131e7779e2e8cc1e7ae25918996f9cb195
Hossein <hosseinpro@gmail.com>
2024-09-27 13:12:15 +0330

    fix filtering bugs.

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -1,27 +1,51 @@
-import prisma from "@/prisma/client";
-import { Table } from "@radix-ui/themes";
-import IssueActions from "./IssueActions";
-import { Status } from "@prisma/client";
-import Link from "@/app/components/Link";
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import Link from "@/app/components/Link";
+import prisma from "@/prisma/client";
+import { Problem, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
+import { Table } from "@radix-ui/themes";
+import NextLink from 'next/link';
+import IssueActions from "./IssueActions";
 
 interface Props {
-    searchParams: { status: Status }
+    searchParams: { status: Status, orderBy: keyof Problem };
 }
 
-const IssuesPage = async ({
-    searchParams,
-}: Props) => {
+const IssuesPage = async ({ searchParams }: Props) => {
+    const columns: {
+        label: string;
+        value: keyof Problem;
+        className?: string;
+    }[] = [
+            { label: "Issue", value: "title" },
+            {
+                label: "Status",
+                value: "status",
+                className: "hidden md:table-cell",
+            },
+            {
+                label: "Created",
+                value: "createdAt",
+                className: "hidden md:table-cell",
+            },
+        ];
 
     const statuses = Object.values(Status);
     const status = statuses.includes(searchParams.status)
         ? searchParams.status
         : undefined;
 
+    const orderBy = columns
+        .map(column => column.value)
+        .includes(searchParams.orderBy)
+        ? { [searchParams.orderBy]: 'asc' }
+        : undefined;
+
     const issues = await prisma.problem.findMany({
         where: {
-            status
-        }
+            status,
+        },
+        orderBy
     });
 
     return (
@@ -30,15 +54,14 @@ const IssuesPage = async ({
             <Table.Root variant="surface">
                 <Table.Header>
                     <Table.Row>
-                        <Table.ColumnHeaderCell>
-                            Issue
-                        </Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Status
-                        </Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Created
-                        </Table.ColumnHeaderCell>
+                        {columns.map((column) => (
+                            <Table.ColumnHeaderCell key={column.value} className={column.className}>
+                                <NextLink href={{
+                                    query: { ...searchParams, orderBy: column.value }
+                                }}>{column.label}</NextLink>
+                                {column.value === searchParams.orderBy && <ArrowUpIcon className="inline" />}
+                            </Table.ColumnHeaderCell>
+                        ))}
                     </Table.Row>
                 </Table.Header>
                 <Table.Body>
